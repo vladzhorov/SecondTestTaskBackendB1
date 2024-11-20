1e66ffcc3808a2e5ea5ace505abf7a183ae1823c
vladzhorov <zhorovv7@gmail.com>
2024-11-20 20:06:32 +0300

    add repository

---
+++ SecondTestTaskB1/Repositories/AccountRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecondTestTaskB1.Db;
+using SecondTestTaskB1.Interfaces;
+using SecondTestTaskB1.Models;
+
+namespace SecondTestTaskB1.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AccountRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Account>> GetAllAccountsAsync()
+        {
+            return await _context.Accounts.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Account>> GetAccountsByFileIdAsync(int fileId)
+        {
+            return await _context.Accounts
+                .Where(account => account.FileId == fileId)
+                .ToListAsync();
+        }
+
+        public async Task AddAccountsAsync(IEnumerable<Account> accounts)
+        {
+            await _context.Accounts.AddRangeAsync(accounts);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ SecondTestTaskB1/Repositories/FileRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecondTestTaskB1.Db;
+using SecondTestTaskB1.Interfaces;
+using File = SecondTestTaskB1.Models.File;
+
+
+namespace SecondTestTaskB1.Repositories
+{
+    public class FileRepository : IFileRepository
+    {
+        private readonly AppDbContext _context;
+
+        public FileRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<File>> GetAllFilesAsync()
+        {
+            return await _context.Files.Include(f => f.Accounts).ToListAsync();
+        }
+
+        public async Task<File?> GetFileByIdAsync(int id)
+        {
+            return await _context.Files.Include(f => f.Accounts).FirstOrDefaultAsync(f => f.Id == id);
+        }
+        public async Task<File?> GetFileByFileNameAsync(string fileName)
+        {
+            return await _context.Files
+                .FirstOrDefaultAsync(f => f.FileName == fileName);
+        }
+
+        public async Task AddFileAsync(File file)
+        {
+            if (file != null)
+            {
+                //Конвертирование под UTF в бд
+                if (file?.UploadDate != null)
+                {
+                    file.UploadDate = file.UploadDate.ToUniversalTime();
+                }
+
+            }
+
+            await _context.Files.AddAsync(file!);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+
+    }
+}
